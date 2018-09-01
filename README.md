4c342a719050beff4276efa72bda902c982bbdf0
Matt Hamlin <hamlim@outlook.com>
2018-09-01 12:45:31 -0400

    Init package

---
+++ README.md
@@ -0,0 +1,13 @@
+# React Sheriff
+
+A React component for making emoji sheriffs.
+
+### Usage
+
+`yarn add react-sheriff` or `npm install react-sheriff`
+
+```jsx
+import Sheriff from 'react-sheriff'
+
+render(<Sheriff emoji="🏈" />)
+```
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Sherif = ({ emoji, withStyles = false } = {}) => (
+  <React.Fragment>
+    {withStyles && <style>{`.sherif{font-size:3rem;}`}</style>}
+    <pre className="sherif">{`
+       🤠
+     ${emoji}${emoji}${emoji}
+    ${emoji} ${emoji} ${emoji}
+   👇  ${emoji}  👇
+  　  ${emoji} ${emoji}
+　   ${emoji}  ${emoji}
+　   👢  👢
+`}</pre>
+  </React.Fragment>
+)
+
+export default Sherif
